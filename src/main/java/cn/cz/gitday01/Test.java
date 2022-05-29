96b4255f9f35a488811d8ac72421702b11304d0b
chuzhuo007 <106295665+chuzhuo007@users.noreply.github.com>
2022-05-29 16:53:15 +0800

    pull commit

--- src/main/java/cn/cz/gitday01/Test.java
+++ src/main/java/cn/cz/gitday01/Test.java
@@ -3,5 +3,6 @@ package cn.cz.gitday01;
 public class Test {
     public static void main(String[] args) {
         System.out.println("push test");
+         System.out.println("pull test");
     }
 }
