7a5481b6d0346404576f7e0af3b408b178a20bda
Cz <1562890483@qq.com>
2022-05-29 14:33:23 +0800

    second commit

---
+++ src/main/java/cn/cz/gitday01/pojo/Person.java
@@ -0,0 +1,13 @@
+package cn.cz.gitday01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Person {
+    private String name;
+    public Integer age;
+}
