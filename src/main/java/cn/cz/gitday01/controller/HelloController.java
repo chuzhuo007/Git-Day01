3287c6c650f081fb8c6c3d6152311f67a39d96de
Cz <1562890483@qq.com>
2022-05-29 14:55:39 +0800

    hot commit

--- src/main/java/cn/cz/gitday01/controller/HelloController.java
+++ src/main/java/cn/cz/gitday01/controller/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 @RequestMapping("/helllo")
 public String hello(){
-        return "hello";
+
+        return "hello hello fix4";
 }
 }
