f7ec2a9936b2ae1c53be230f118258061ec42fbd
Cz <1562890483@qq.com>
2022-05-29 14:30:10 +0800

    first commit

---
+++ src/main/java/cn/cz/gitday01/GitDay01Application.java
@@ -0,0 +1,13 @@
+package cn.cz.gitday01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDay01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDay01Application.class, args);
+    }
+
+}
